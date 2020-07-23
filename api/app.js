24979017566ddc425fddf8a556ec2e518c7fc960
Amy Wang <58236743+amywang480@users.noreply.github.com>
2020-07-22 20:23:41 -0400

    Updated gitignore

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
+var key = require('./apiKey');
 
 // require express to use routes
 var indexRouter = require('./routes/index');
